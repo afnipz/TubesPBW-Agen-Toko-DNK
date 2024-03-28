1afc1123981e49d3b37603ee78fb528379a8f22d
Ghumaisa <ghumaisaa@gmail.com>
2024-03-28 21:37:46 +0700

    Lanjutan Prak03 - Tugas03 Anonymous dan Closure

--- pbw-praktikum-pertemuan03/Praktikum02_4522210131_Siti Ghumaisa.go
+++ pbw-praktikum-pertemuan03/Praktikum02_4522210131_Siti Ghumaisa.go
@@ -4,13 +4,13 @@ package main
 import "fmt"
 
 func main() {
-
     batasNilai := map[string]int{
         "A": 90,
         "B": 80,
         "C": 70,
         "D": 60,
     }
+
     // closure untuk menentukan nilai huruf berdasarkan nilai ujian
     getNilaiHuruf := func(nilaiUjian int) string {
         for huruf, batas := range batasNilai {
@@ -20,6 +20,7 @@ func main() {
         }
         return "E"
     }
+
     // menentukan nilai huruf untuk beberapa nilai ujian
     nilaiUjian := []int{85, 75, 95, 55, 65}
 	for _, nilai := range nilaiUjian {
