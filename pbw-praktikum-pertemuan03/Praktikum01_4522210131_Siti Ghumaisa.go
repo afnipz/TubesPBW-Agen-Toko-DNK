21235656459d646d3a104e94fab10537f2e85b1b
Ghumaisa <ghumaisaa@gmail.com>
2024-03-28 20:42:20 +0700

    Lanjutan Tugas 03 Anonymouse dan Closure

---
+++ pbw-praktikum-pertemuan03/Praktikum01_4522210131_Siti Ghumaisa.go
@@ -0,0 +1,28 @@
+// anonymous function
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main (){
+
+	jariJari := 5.0
+
+    // Menghitung luas dan keliling dengan anonymous function
+    luas := func(r float64) float64 {
+        return math.Pi * r * r
+    }(jariJari)
+	
+	keliling := func (r float64) float64 {
+			return 2 * math.Pi * r
+	}(jariJari)
+
+	// Menampilkan hasil
+	fmt.Println()
+	fmt.Println("Luas Lingkaran :", luas)
+	fmt.Println("Keliling Lingkaran :", keliling)
+	fmt.Println()
+}
